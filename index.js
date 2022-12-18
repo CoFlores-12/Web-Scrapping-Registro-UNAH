342c2f4deb7aea541f67496f7f490cae303e5532
CoFlores-12 <obethflores2014@gmail.com>
2022-12-17 20:21:56 -0600

    test 01.6

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const app = express();
 
 app.set('port', 8000);
 
-var browser = null;
-var page = null;
+let browser = null;
+let page = null;
+let pages = 0;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function miMiddleware(req,res,next){
-  const start = new Date();
+async function openRegister(req,res,next){
   if (browser === null) {
       browser = await chrome.puppeteer.launch({
           args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
@@ -26,35 +26,41 @@ async function miMiddleware(req,res,next){
   //go to login page
   page = await browser.newPage();
   await page.goto('https://registro.unah.edu.hn/pregra_estu_login.aspx');
-  const end = new Date() - start;
-  console.log(`Tiempo de ejecución ${end} ms`);
   await next();
 }
 
-app.get('/api/:cuenta/:clave', miMiddleware, async function (req, res) {
-  
-  //JSON response
-  const classRes = [];
-
+async function login(req,res,next){
   //login with credentials 
   await page.type('#MainContent_txt_cuenta', req.params['cuenta']);
   await page.type('#MainContent_txt_clave', req.params['clave']);
   await page.click('#MainContent_Button1');
-  
+
   //go to history
   await page.waitForSelector('#MainContent_LinkButton2');
+  await next();
+}
+
+async function pageNumber(req,res,next){
+  
   await page.click('#MainContent_LinkButton2');
 
   await page.waitForSelector('#MainContent_ASPxPageControl1_ASPxGridView2_DXMainTable');
 
   //get number of pages in history
-  let pages = await page.evaluate(() => {
+  pages = await page.evaluate(() => {
     const data = document.getElementsByClassName('dxpSummary_Aqua');
     const myArray = data[0].innerHTML.split(" ");
     return myArray[3];
   });
   
+  await next();
+}
+
+app.get('/api/:cuenta/:clave', openRegister, login, pageNumber, async function (req, res) {
   
+  //JSON response
+  const classRes = [];
+
   for (let i = 0; i < pages; i++) {
     const classRestmp = await page.evaluate(() => { 
       const  clases = [];
@@ -94,6 +100,7 @@ app.get('/api/:cuenta/:clave', miMiddleware, async function (req, res) {
   classRes.push(promedio);
 
   await page.close();
+  page = null;
   
   res.send(classRes);
   
